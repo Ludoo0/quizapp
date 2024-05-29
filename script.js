7547f1f513d73947d3c48aa577b71faeb6ab0789
Ludoo <ludoo0215@gmail.com>
2024-05-29 19:16:45 +0200

    added SaveQuestion form and script

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function loginAnswer(number){
-    alert('Test')
+function loginAnswer(number) {
+}
+function SaveQuestion() {
+  let Question = {
+    Q: document.getElementById("Q").value,
+    A1: document.getElementById("A1").value,
+    A2: document.getElementById("A2").value,
+    A3: document.getElementById("A3").value,
+    RA: document.getElementById("Q").value,
+  };
+  const arr = [Question.Q, Question.A1, Question.A2, Question.A3, Question.RA];
+  let length_int = localStorage.length + 1;
+  let length = length_int.toString();
+  localStorage.setItem(length, JSON.stringify(arr));
 }
